e5308db8551714e4cd7c083071e1cb6924445b6c
negue <negue@users.noreply.github.com>
2020-11-30 22:40:57 +0000

    deploy: 177b0b13858e149ccf03544ec89c5c874f334932

---
+++ precache-manifest.82ddc4dcf398251b50c679b1a0a4befb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5e379f014ad934abab",
+    "url": "/css/app.5499c4da.css"
+  },
+  {
+    "revision": "4b1fb93682403abefa89961eac21f923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df5e379f014ad934abab",
+    "url": "/js/app.80c9321b.js"
+  },
+  {
+    "revision": "cb02cba3f46a9ca2ef05",
+    "url": "/js/chunk-vendors.bf516917.js"
+  },
+  {
+    "revision": "2e747b05a7d5684ba31506d28f79c492",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
