15c97aa4cec97be4c4e0f6c1165907141d67edd2
negue <eugen.bolz@gmail.com>
2021-06-13 17:22:06 +0200

    Add ScrollingContent + Modal Component

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <vue-final-modal v-model="opened">
+    <div class="nes-dialog is-dark" v-if="opened">
+      <closable-panel @cancel="opened = false">
+        <slot></slot>
+      </closable-panel>
+    </div>
+  </vue-final-modal>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import { Prop } from 'vue-property-decorator';
+import ClosablePanel from '@/components/ClosablePanel.vue';
+
+@Options({
+  components: {
+    ClosablePanel
+
+  }
+})
+export default class Modal extends Vue {
+  @Prop({ default: false }) opened!: boolean;
+}
+</script>
+
+<style scoped>
+
+</style>
